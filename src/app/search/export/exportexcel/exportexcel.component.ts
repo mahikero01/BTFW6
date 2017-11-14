aa027baf5e01e126003ed629027d3adaccdb8c46
Sarmiento <sarmife@prd.manulifeusa.com>
2017-11-14 13:40:38 -0500

    up

--- src/app/search/export/exportexcel/exportexcel.component.ts
+++ src/app/search/export/exportexcel/exportexcel.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exportexcel.component.css']
 })
 export class ExportexcelComponent implements OnInit {
-
+  public testResult = true && true || 'heello';
   constructor() { }
 
   ngOnInit() {
